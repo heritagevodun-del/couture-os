4b9a08665670619c09d9e76e04f078e85b431288
Heritage Vodun <heritagevodun@gmail.com>
2026-01-16 15:17:20 +0100

    chore(pwa): installation module pwa et fix build webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  // swcMinify: true, <--- J'ai supprimé cette ligne qui causait l'erreur
+  disable: process.env.NODE_ENV === "development", // Désactive la PWA en mode dev
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Vos autres configurations Next.js iront ici si besoin
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
